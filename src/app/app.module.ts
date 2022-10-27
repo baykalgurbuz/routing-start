6d9530e8c4b9186af53a0dea79b7c1e08b5457c8
Enes Baykal Gürbüz <ebaykalgurbuz@gmail.com>
2022-10-27 19:19:55 +0300

    Fetching route parametres

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { Routes ,RouterModule} from '@angular/router';
 const appRoutes:Routes=[
   { path:'' ,component:HomeComponent}, 
   { path:'users' ,component:UsersComponent}, 
-  { path:'users/:id' ,component:UsersComponent}, 
+  { path:'users/:id/:name' ,component:UsersComponent}, 
   { path:'servers' ,component:ServersComponent} 
 ];
 @NgModule({

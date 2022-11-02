1a15971e0f01b591c9e0707ad2e82f847022eddd
Enes Baykal Gürbüz <ebaykalgurbuz@gmail.com>
2022-11-02 16:20:21 +0300

    Passing Query params ,redirecting wilcard routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { UsersComponent } from "./users/users.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { ServersService } from "./servers/servers.service";
+ 
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from "./app-routing.module";
 
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
-import { ServersService } from './servers/servers.service';
-import { Routes ,RouterModule} from '@angular/router';
-
-const appRoutes:Routes=[
-  { path:'' ,component:HomeComponent}, 
-  { path:'users' ,component:UsersComponent}, 
-  { path:'users/:id/:name' ,component:UsersComponent}, 
-  { path:'servers/:id' ,component:ServerComponent} ,
-  { path:'servers/:id/edit' ,component:EditServerComponent} 
-];
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,14 +23,11 @@ const appRoutes:Routes=[
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes),
-  ],
+  imports: [BrowserModule, FormsModule,AppRoutingModule],
   providers: [ServersService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
